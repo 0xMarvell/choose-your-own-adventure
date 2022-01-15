bd4e905f51d01534cd4117a4065b502c854cb751
Marvellous Chimaraoke <rokemarvellous@gmail.com>
2022-01-15 20:56:13 +0100

    created structs for unmarshalling JSON

--- story.go
+++ story.go
@@ -3,9 +3,9 @@ package cyoa
 type Story map[string]Arc
 
 type Arc struct {
-	Title   string   `json:"title"`
-	Story   []string `json:"story"`
-	Options []Option `json:"options"`
+	Title      string   `json:"title"`
+	Paragraphs []string `json:"story"`
+	Options    []Option `json:"options"`
 }
 
 type Option []struct {
