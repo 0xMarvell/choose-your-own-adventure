1887a4c90fb876a56d1193c823244cd8e5efc859
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-14 18:11:48 +0100

    Built JsonStory func to handle parsing json

--- story.go
+++ story.go
@@ -1,5 +1,21 @@
 package cyoa
 
+import (
+	"encoding/json"
+	"io"
+
+	cyoa "github.com/Marvellous-Chimaraoke/choose-your-own-adventure"
+)
+
+func JsonStory(r io.Reader) (Story, error) {
+	d := json.NewDecoder(r)
+	var story cyoa.Story
+	if err := d.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
+}
+
 type Story map[string]Chapter
 
 type Option struct {
