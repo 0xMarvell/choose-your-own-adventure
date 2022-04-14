8a04c6d025bafa1c61f805ef5688d16f33e2efcf
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-14 17:50:10 +0100

    refactor

--- Web/cmd/cyoaweb/main.go
+++ Web/cmd/cyoaweb/main.go
@@ -9,5 +9,5 @@ func main() {
 	filename := flag.String("file", "gopher.json", "The JSON file containing the 'Choose Your Own Adventure' story")
 	flag.Parse()
 
-	fmt.Printf("Using the story in %s", *filename)
+	fmt.Printf("Using the story in %s...\n", *filename)
 }
